a0fda96d2b08cafbc364f2007c0051074c5badac
truefusion <truefusion@gmail.com>
2022-09-04 19:12:11 -0400

    Update Bread.vue

--- src/views/Bread.vue
+++ src/views/Bread.vue
@@ -11,7 +11,7 @@
 
 <style lang="less">
     #bread {
-        @apply p-6 md:m-auto md:px-0 md:py-12 md:w-1/2;
+        @apply p-6 md:m-auto md:px-0 md:py-24 md:w-1/2;
 
         header {
             @apply text-4xl;
