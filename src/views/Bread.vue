f36b28aedda1319a2a3ceca03d9c428131fde7b5
truefusion <truefusion@gmail.com>
2022-09-05 19:23:50 -0400

    Update Bread.vue
    Margins.

--- src/views/Bread.vue
+++ src/views/Bread.vue
@@ -29,7 +29,7 @@
         }
 
         .modules {
-            @apply cursor-pointer gap-5 grid grid-cols-1 lg:gap-8 lg:grid-cols-2 xl:grid-cols-3;
+            @apply cursor-pointer gap-5 grid grid-cols-1 lg:gap-8 lg:grid-cols-2 lg:-mx-3 xl:grid-cols-3;
 
             .module {
                 @apply duration-300 flex flex-col rounded text-lg transition lg:p-3 lg:h-24;
