9952a60cffbe49bb84c0d52cd86a412317d675c3
truefusion <truefusion@gmail.com>
2022-09-15 15:54:18 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+import '@mdi/font/css/materialdesignicons.css';
 import './styles/base.css';
 
 const app = createApp(App);
