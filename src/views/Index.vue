d662ea763b482604e175950b505fc0212b7de442
truefusion <truefusion@gmail.com>
2022-09-19 05:03:01 -0400

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -30,7 +30,7 @@
 
 <style lang="less">
     #index {
-        @apply bg-center bg-cover bg-no-repeat fixed flex flex-col inset-0 md:bg-center md:items-center md:justify-center;
+        @apply bg-center bg-cover bg-no-repeat fixed flex flex-col inset-0 z-20 md:bg-center md:items-center md:justify-center;
         background-image: url(/images/bg.avif);
 
         &::before {
